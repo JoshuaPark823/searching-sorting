147b4600244795014635e345c330061aca07f9cb
Joshua Park <joshuapark823@gmail.com>
2021-11-08 20:35:14 -0500

    material design

--- searching-sorting/src/app/app.module.ts
+++ searching-sorting/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridMainComponent } from './components/grid-main/grid-main.component';
 import { SortMainComponent } from './components/sort-main/sort-main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { SortMainComponent } from './components/sort-main/sort-main.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
