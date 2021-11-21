00ba3d9a4c94170e3b84a4f644e109bbbc220caf
Joshua Park <joshuapark823@gmail.com>
2021-11-20 19:38:36 -0500

    remove component

--- searching-sorting/src/app/app.module.ts
+++ searching-sorting/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GridMainComponent } from './components/grid-main/grid-main.component';
-import { SortMainComponent } from './components/sort-main/sort-main.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchViewComponent } from './components/search-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GridMainComponent,
-    SortMainComponent
+    SearchViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ searching-sorting/src/app/components/search-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-view',
+  templateUrl: './search-view.html',
+  styleUrls: ['./search-view.scss']
+})
+export class SearchViewComponent {
+  title = 'searching';
+}
