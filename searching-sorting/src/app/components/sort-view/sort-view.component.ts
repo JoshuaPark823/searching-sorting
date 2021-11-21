9400295038adfad1a16640ab8a0b09ffcdbc9608
Joshua Park <joshuapark823@gmail.com>
2021-11-21 11:46:04 -0500

    test

--- searching-sorting/src/app/components/sort-view/sort-view.component.ts
+++ searching-sorting/src/app/components/sort-view/sort-view.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SortViewComponent implements OnInit {
 
+  sortableItems = [0, 1, 2, 3, 4, 5];
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  test(): void {
+    this.swap(1, 4);
+  }
+
+  swap(index1: any, index2: any): void {
+    [this.sortableItems[index1], this.sortableItems[index2]] = [this.sortableItems[index2], this.sortableItems[index1]]
   }
 
 }
