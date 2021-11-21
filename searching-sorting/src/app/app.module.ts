6a783fbe2404e08c266331a59d53915d512f4e03
Joshua Park <joshuapark823@gmail.com>
2021-11-21 09:31:36 -0500

    sort

--- searching-sorting/src/app/app.module.ts
+++ searching-sorting/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SearchViewComponent } from './components/search-view.component';
+import { SearchViewComponent } from './components/search-view/search-view.component';
+import { SortViewComponent } from './components/sort-view/sort-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchViewComponent
+    SearchViewComponent,
+    SortViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ searching-sorting/src/app/components/sort-view/sort-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-view',
+  templateUrl: './sort-view.component.html',
+  styleUrls: ['./sort-view.component.scss']
+})
+export class SortViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
